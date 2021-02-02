0074a3a1a31b7fa8a3b3e8e772a03f3ad01d28c4
joeisit <joseramon.riesgo@hotmail.com>
2021-02-01 21:14:06 -0600

    New info

--- code.R
+++ code.R
@@ -1,1 +1,9 @@
 # CODE 
+# Cat s like print
+
+# pull: pull changes from remote repo (if you are in collaboration with others)
+# add: stage files for commit; click on stage box under git pane
+# commit: commit to the local repo; click on "commit" button under git pane; 
+# add a commit message
+# push: push to the remote repo on Github
+
